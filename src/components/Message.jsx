beeef8e9eae1b499ab946d7e1735b0160d5abaa2
Jagdish Saini <jsaini8941@gmail.com>
2021-05-26 17:30:09 +0530

    second commit

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { auth } from '../firebase/firebase';
-import { Card , CardContent, Typography } from '@material-ui/core';
+import { ListItemText } from '@material-ui/core';
 import Zoom from 'react-reveal/Zoom';
 import './Message.css';
 
@@ -14,16 +14,7 @@ const Message = ({ message, username }) => {
             <Zoom>
                 <div className={`message ${isUser && "message__user"}`}>
                     { !isUser && <p style={{fontSize:"10px", marginBottom:"0"}}>{username.split(" ")[0]}</p>}
-                    <Card className={isUser ? "message__user" : "message__guest"}>
-                        <CardContent>
-                            <Typography
-                            variant="h5"
-                            component="h2"
-                            >   
-                                {message}
-                            </Typography>
-                        </CardContent>
-                    </Card>
+                    <ListItemText  primary={message} className={isUser ? "message__user" : "message__guest"}  />
                 </div>
             </Zoom>
         </>
